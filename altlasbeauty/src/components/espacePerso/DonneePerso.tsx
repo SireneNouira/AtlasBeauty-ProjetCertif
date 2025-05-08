f92f76bd7fd1ba604364a2dd4c65f0689a614b2f
SireneNouira <sirene.nouira16@gmail.com>
2025-05-08 14:45:42 +0200

    push pour passer autre pc

---
+++ altlasbeauty/src/components/espacePerso/DonneePerso.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DonneePerso() {
+  return (
+    <div>DonneePerso</div>
+  )
+}
+
+export default DonneePerso
--- altlasbeauty/src/components/espacePerso/Sidebar.tsx
+++ altlasbeauty/src/components/espacePerso/Sidebar.tsx
@@ -8,6 +8,7 @@ import {
   FaBell,
   FaPlus,
   FaSignOutAlt,
+  
 } from 'react-icons/fa'
 
 export default function Sidebar() {
@@ -45,6 +46,11 @@ export default function Sidebar() {
           <FaPlus className="w-5 h-5 mr-3" />
           Nouvelle demande
         </Link>
+
+        <Link href="/patient/chat"
+          className="flex items-center px-4 py-2 text-gray-600 hover:bg-gray-100 rounded-lg transition"
+          >        Message
+                  </Link>
       </nav>
 
       <Link
